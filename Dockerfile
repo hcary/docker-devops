0b7cbce220db3b48a84a1c584298f96f2070fce2
Harvey Cary <hdcary3@gmail.com>
2019-02-08 20:00:38 -0500

    Store chef DK name and versions in variables Pass chfdk infor as a build argument

--- Dockerfile
+++ Dockerfile
@@ -8,17 +8,17 @@ ARG version
 ARG maintainer 
 ARG author
 ARG company
-
+ARG chefdk_file
 
 RUN apt-get update
 RUN apt-get install -y software-properties-common 
 RUN apt-add-repository -y ppa:ansible/ansible
 RUN apt-get update
-RUN apt-get install -y ansible
+RUN apt-get install -y ansible git
 ADD user-setup.sh /usr/local/bin/user-setup.sh
 RUN chmod +x /usr/local/bin/user-setup.sh
 
-ADD chefdk_3.7.23-1_amd64.deb /tmp
-RUN apt-get install -y /tmp/chefdk_3.7.23-1_amd64.deb
+ADD ${chefdk_file} /tmp
+RUN apt-get install -y /tmp/${chefdk_file}
 
 ENTRYPOINT /usr/local/bin/user-setup.sh
--- build.sh
+++ build.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-if [ ! -e chefdk_3.7.23-1_amd64.deb ];
-then
-    curl https://packages.chef.io/files/stable/chefdk/3.7.23/ubuntu/18.04/chefdk_3.7.23-1_amd64.deb -o chefdk_3.7.23-1_amd64.deb 
-fi
-
 build_num=`cat build_num.txt`
 newnum=`expr $build_num + 1`
 echo $newnum >  build_num.txt
 
 export build_num=$newnum
 
+chefdk_ver="3.7.23-1"
+chefdk_file="chefdk_${chefdk_ver}_amd64.deb"
+chefdk_url="https://packages.chef.io/files/stable/chefdk/${chefdk_ver}/ubuntu/18.04/${chefdk_file}"
+
+if [ ! -e ${chefdk_file} ];
+then
+    curl ${chefdk_url} -o ${chefdk_file} 
+fi
+
+
 
 docker build -t rc3labs/ansible . \
- --label "build=${build_num}" \
- --label "version=1.0" \
- --label "maintainer"="harvey.cary@rc3labs.com" \
- --label "author"="Harvey Cary (hcary@rc3labs.com)" \
- --label "company"="rc3labs.com" 
+    --build-arg "chefdk_file=${chefdk_file}" \
+    --label "build=${build_num}" \
+    --label "version=1.0" \
+    --label "maintainer"="harvey.cary@rc3labs.com" \
+    --label "author"="Harvey Cary (hcary@rc3labs.com)" \
+    --label "company"="rc3labs.com" 
 
